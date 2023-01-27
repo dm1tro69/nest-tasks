d2d32ae5da9bfe198cc626a69d90db3a1f991986
Dmytro Voronov <dimolg22@gmail.com>
2023-01-27 16:39:38 +0200

    add crud

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,28 +1,29 @@
 import {Injectable} from "@nestjs/common";
 import { PrismaService } from "../prisma/prisma.service";
 import { TasksDto } from "./dto/tasks.dto";
+import { Tasks } from "@prisma/client";
 
 @Injectable()
 export class TasksService {
   constructor(private readonly prisma: PrismaService) {
   }
-  getAllTasks(){
-
+  async getAllTasks(): Promise<Tasks[]>{
+    return this.prisma.tasks.findMany()
   }
 
-  getTasksById(id: number){
-
+  async getTasksById(id: number): Promise<Tasks>{
+    return this.prisma.tasks.findUnique({where: {id}})
   }
 
-  createTask(dto: TasksDto){
-
+  async createTask(dto: TasksDto): Promise<Tasks>{
+    return this.prisma.tasks.create({data: dto})
   }
 
-  deleteTask(id: number){
-
+  async deleteTask(id: number): Promise<Tasks>{
+     return this.prisma.tasks.delete({where: {id}})
   }
 
-  updateTaskStatus(id: number, dto: TasksDto){
-
+  async updateTaskStatus(id: number, dto: TasksDto): Promise<Tasks>{
+      return this.prisma.tasks.update({where: {id}, data: dto})
   }
 }
